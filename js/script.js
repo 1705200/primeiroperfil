e096f887d4798231554ba6aac69f2cedfdc11a51
Amanda Brito <112962708+1705200@users.noreply.github.com>
2023-10-28 15:16:48 -0300

    aula 6
     finalizado

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 //tres constantes para referenciar os ITEM nas Divs Projetos 
-const album = document.getElementById('AlbumFotos');
-const site = document.getElementById('Casamentos');
-const port = document.getElementById('Buffet');
+// sinal = usado para atribuir algo
+//Document. identifica a parta aberta no servidor 
+const albumFotos = document.ge
 
-const videoAlbum = document.querySelector('.videoAlbum');
-const videoCasamentos = document.querySelector('.videoCasamentos');
-const videoBuffet = document.querySelector('.videoBuffet');
+
+const album = document.getElementById('album');
+const casamentos = document.getElementById('Casamentos');
+const buffet = document.getElementById('Buffet');
+
+//tres constantes para referenciar os IDS  
+
+const videoAlbum = document.querySelector('#videoAlbum');
+const videoCasamentos = document.querySelector('#videoCasamentos');
+const videoBuffet = document.querySelector('#videoBuffet');
 
 //Evento de click
 album.addEventListener('click',() =>{
     album.classList.toggle('active');
+    //para iframe
     videoAlbum.classList.toggle('active');
 });
-site.addEventListener('click', () =>{
-    site.classList.toggle('active');
+casamentos.addEventListener('click', () =>{
+    casamentos.classList.toggle('active');
     videoCasamentos.classList.toggle('active');
 });
-port.addEventListener('click', () =>{
-    port.classList.toggle('active');
+buffet.addEventListener('click', () =>{
+    buffet.classList.toggle('active');
     videoBuffet.classList.toggle('active');
 });
-
-
-
